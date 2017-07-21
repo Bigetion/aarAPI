95ec1d6b11139d3fd7293b9a0f10c9bdd0a306a7
Bigetion <adysuprianto@yahoo.co.id>
2017-07-21 20:24:01 +0700

    First Commit

---
+++ application/config/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	'base_url'						=> 'http://localhost/aarAPI/',
+	'default_app_method'			=> 'index',
+	'default_project'				=> 'page',
+	'default_project_controller'	=> 'home',
+	'default_project_method'		=> 'index',
+	'secret_key'					=> '123456789'
+)
+?>
---
+++ application/controllers/login.php
@@ -0,0 +1,44 @@
+<?php  if ( ! defined('INDEX')) exit('No direct script access allowed');
+
+class Login extends Main {
+
+    function index(){
+        $json_data = $this->render->json_post();
+        $user = strtolower($json_data['username']);
+        $password = $json_data['password'];
+
+        if (empty($user)|| empty($password))
+            $this->set->error_message("Username atau password harus diisi.!");
+            
+        $data = $this->db->select("users","*",["username"=>$user]);
+        if (count($data) == 0)
+            $this->set->error_message('Username dan Password salah..!');
+        else {
+            if(!password_verify($password,$data[0]["password"])) $this->set->error_message("Username dan password tidak cocok.!");
+            else{
+                try{
+                    $payload = array(
+                        'jti'       => bin2hex(random_bytes(5)),
+                        'iat'       => time(),
+                        'nbf'       => time() + 10,
+                        'exp'       => time() + 7210,
+                        'iss'       => get_header('origin'),
+                        'data'      => array(
+                                    'user'  => strtolower($user),
+                                    )
+                    );
+                    $jwtTokenEncode = $this->jwt->encode($payload, base64_decode(secret_key));
+
+                    $token['jwt'] = $jwtTokenEncode;
+                    
+                    $this->set->success_message(true, $token);
+                }
+                catch(Exception $ex){
+                    $this->set->error_message(true, $ex);
+                }
+            }
+        }
+    }
+}
+
+?>
