dd472b8383deebeca4554dec6787d18eabc975e8
Bigetion <adysuprianto@yahoo.co.id>
2018-11-01 17:39:07 +0700

    Update Sleek Get Data Config

--- project/base/controllers/sleek.php
+++ project/base/controllers/sleek.php
@@ -4,29 +4,44 @@ class sleek extends Controller {
 		$post_data = $this->render->json_post();
 		$name = $post_data['name'];
 		$json_data = null;
-		if(file_exists('project/base/config/sleek/query/'.id_role.'/'.$name.'.json')){
-			$json_data = json_decode(file_get_contents('project/base/config/sleek/query/'.id_role.'/'.$name.'.json'), true);
-		} else if(file_exists('project/base/config/sleek/query/'.$name.'.json')){
-			$json_data = json_decode(file_get_contents('project/base/config/sleek/query/'.$name.'.json'), true);
+		if(file_exists('project/base/config/sleek-query/'.id_role.'/'.$name.'.json')){
+			$json_data = json_decode(file_get_contents('project/base/config/sleek-query/'.id_role.'/'.$name.'.json'), true);
+		} else if(file_exists('project/base/config/sleek-query/'.$name.'.json')){
+			$json_data = json_decode(file_get_contents('project/base/config/sleek-query/'.$name.'.json'), true);
 		}
 		$data= array();
+		
 		if(!empty($json_data)) {
-			if(is_array($json_data['query'])){
-				$array_keys = array_keys($json_data['query']);
-				if($array_keys[0] === 0){
-					$query = $json_data['query'];
-					foreach($query as $key=>$q){
-						$store = $q['store'];
-						$keys = $q['keys'];
-						$this->sleekdb->setStore($store);
-						$data['data'][] =  $this->sleekdb->store->fetch($keys);
+			if(isset($json_data['query'])){
+				$query = $json_data['query'];
+				foreach($query as $key=>$q){
+					$store = $q['store'];
+					$keys = $q['keys'];
+					$where = array();
+					
+					$tmpData = $this->sleekdb->select($store, $keys, $where);
+					$tmpTotalRows = $this->sleekdb->totalRows();
+					
+					if(isset($post_data['options'])){
+						if(isset($post_data['options'][$key])){
+							$options = $post_data['options'][$key];
+							if(isset($options['where'])) {
+								$where = $options['where'];
+							}
+							$tmpData = $this->sleekdb->select($store, $keys, $where);
+							$tmpTotalRows = $this->sleekdb->totalRows();
+							if(isset($options['limit'])) {
+								$limit = $options['limit'];
+								$where[] = ["limit" => $limit];
+							}
+							if(isset($options['sortBy'])) {
+								$sortBy = $options['sortBy'];
+								$where[] = ["sortBy" => $sortBy];
+							}
+							$tmpData = $this->sleekdb->select($store, $keys, $where);
+						};
 					}
-				} else {
-					$query = $json_data['query'];
-					$store = $query['store'];
-					$keys = $query['keys'];
-					$this->sleekdb->setStore($store);
-					$data['data'] =  $this->sleekdb->store->fetch($keys);
+					$data['data'][] = array("rows" => $tmpData, "total" => $tmpTotalRows);
 				}
 			}
 		}
