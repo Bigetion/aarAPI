d6a5759e563081471d41533569e7c44076f58bc4
Bigetion <adysuprianto@yahoo.co.id>
2019-05-06 16:22:58 +0700

    Fix

--- application/config/example.config.php
+++ application/config/example.config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-	'base_url'						=> 'http://localhost/aarAPI/',
-	'default_app_method'			=> 'index',
-	'default_project'				=> 'page',
+	'base_url'										=> 'http://' . $_SERVER['HTTP_HOST'] . '/aarAPI/',
+	'default_app_method'					=> 'index',
+	'default_project'							=> 'page',
 	'default_project_controller'	=> 'home',
-	'default_project_method'		=> 'index',
-	'secret_key'					=> '123456789',
-	'allowed_origin' => '*',
+	'default_project_method'			=> 'index',
+	'secret_key'									=> '123456789',
+	'allowed_origin' 							=> '*',
 )
 ?>
