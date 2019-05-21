27e241c8c80e30659b2d8d96b7a87fb40cfb0895
Bigetion <adysuprianto@yahoo.co.id>
2019-05-21 18:28:22 +0700

    Fix

--- application/config/example.config.php
+++ application/config/example.config.php
@@ -1,6 +1,9 @@
 <?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+$path = str_replace('\\', '/', getcwd());
+$base_path = str_replace($root, '', $path);
 return array(
-	'base_url'										=> 'http://' . $_SERVER['HTTP_HOST'] . '/aarAPI/',
+	'base_url'										=> 'http://' . $_SERVER['HTTP_HOST'] . $base_path . '/',
 	'default_app_method'					=> 'index',
 	'default_project'							=> 'page',
 	'default_project_controller'	=> 'home',
