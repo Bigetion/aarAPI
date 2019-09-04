117c002e1b4cb7e8b7f27fe045d8a8836b0a0355
Bigetion <adysuprianto@yahoo.co.id>
2019-09-04 22:04:53 +0700

    Fix New Error

--- project/base/controllers/table.php
+++ project/base/controllers/table.php
@@ -44,7 +44,8 @@ class table extends Controller {
 			$json_data = $json_data['query'];
 			$table = $json_data['table'];
 			$column = $json_data['column'];
-			$where = $post_data['where'];
+			$where = array();
+			if(isset($post_data['where'])) $where = $post_data['where'];
 	
 			if(isset($json_data['join'])){
 				$join = $json_data['join'];
